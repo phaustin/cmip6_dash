a87e560e5efcc6d64521f25334ecaae3d98bb75a
jacobmcf <jacobmcf@student.ubc.ca>
2021-04-15 08:55:18 -0700

    Fix hardcoding in plot_fcns; integrate into app

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ import gcsfs
 import cartopy.crs as ccrs
 from pathlib import Path
 import pandas as pd
-from cmpi_6_dash.src.a448_lib import data_read
-from cmpi_6_dash.src.plot_fcns import *
+from src.a448_lib import data_read
+from src.plot_fcns import *
 import fsspec
 import cmocean as cm
 import cartopy.feature as cfeature
@@ -24,7 +24,7 @@ import numpy as np
 from io import BytesIO
 import base64
 
-# Code from jupyter notebook
+# Checking to see if the data is already downloaded
 csv_filename = "pangeo-cmip6.csv"
 root = "https://storage.googleapis.com/cmip6"
 if Path(csv_filename).is_file():
@@ -35,77 +35,19 @@ else:
     
 json_filename="https://storage.googleapis.com/cmip6/pangeo-cmip6.json"
 
+# Getting the esm data store
 catalog_df=pd.read_csv(csv_filename)
 
 col = intake.open_esm_datastore(json_filename)
 
-# Creating variable key dict constant
-var_key = {
-    'tas' : 
-        {'fullname' : 'Near-Surface Air Temperature',
-         'monthly_table' : 'Amon',
-         'units' : '[K]'},
-    'ta' :
-        {'fullname' : 'Air Temperature',
-         'monthly_table' : 'Amon',
-         'units' : '[K]'},
-    'pr' : 
-        {'fullname' : 'Precipitation',
-         'monthly_table' : 'Amon',
-         'units' : '[kg m-2 s-1]'},
-    'hus' : 
-        {'fullname' : 'Specific Humidity',
-         'monthly_table' : 'Amon' ,
-         'units' : '[1]'}, 
-    'cl' : 
-        {'fullname' : 'Percentage Cloud Cover',
-         'monthly_table' : 'Amon',
-         'units' : '[%]'},
-    'sisnthick' :
-        {'fullname' : 'Snow Thickness',
-         'monthly_table' : 'SImon',
-         'units' : '[m]'},
-    'sithick' :  
-        {'fullname' : 'Sea Ice Thickness',
-         'monthly_table' : 'SImon',
-         'units' : '[m]'},
-    'mrro' : 
-        {'fullname' : 'Total Runoff', 
-         'monthly_table' : 'Lmon',
-         'units' : '[kg m-2 s-1]'},
-    'lai' : 
-        {'fullname' : 'Leaf Area Index',
-         'monthly_table' : 'Lmon',
-         'units' : '[1]'}, 
-    'mrso' : {'fullname' : 'Total Soil Moisture Content' ,
-              'monthly_table' : 'Lmon',
-              'units' : '[kg m-2]'}
-}
+var_key = get_var_key()
 
-# Creating object
+# Creating object with all variable full names
 full_name_key = []
 for var in var_key:
     full_name_key.append({'label' : var_key[var]['fullname'], 'value' : var})
 
-# Helper functions
-def get_monthly_table_for_var(var_id):
-    '''Returns appropriate X-mon for a given variable id from
-       the var key'''
-    return var_key[var_id]['monthly_table']
-
-
-
-def get_models_with_var(data_store, var_id, table_id):
-    '''Takes a variable id and a corresponding table id and and returns all the model labels 
-       with the combination '''
-    query_variable_id = dict(
-        experiment_id=['historical'],
-        table_id = [table_id],
-        variable_id=[var_id])
-
-    data_sets = data_store.search(**query_variable_id)
-    return(data_sets.df.source_id.unique())
-
+# Creating object with all models for given variables
 models = get_models_with_var(data_store = col, var_id = 'tas', table_id = get_monthly_table_for_var('tas'))
 model_list = []
 for mod in models:
@@ -162,7 +104,7 @@ app.layout = dbc.Container([
                     dbc.Card([
                         dbc.CardHeader('Climate Plot', style={'fontWeight': 'bold'}),
                         dbc.CardBody(
-                            html.Img(
+                            dcc.Graph(
                                 id='histogram',
                                 style={'border-width': '0', 'width': '100%', 'height': '100%'}
                                 ))
@@ -176,14 +118,13 @@ app.layout = dbc.Container([
 
 # Callbacks
 @app.callback(
-    Output('histogram', "src"),
+    Output('histogram', "figure"),
     Input('var_drop', "value"))
 def update_map(var_drop):
     var_id = var_drop
     mod_id = 'GFDL-CM4'
-    dset = get_cmpi6_model_run(data_store = col, var_id = var_id,
-     mod_id = mod_id)
-    return plot_year(dset, var_id = var_id, month = '01', year = '1875', layer = 4)
+    fig = plotly_wrapper(col, var_id, mod_id, month = '01', year = '1950', layer = 1)
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
